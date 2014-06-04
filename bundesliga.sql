685081d92708f1bc0ee9213a48e94aac0f2b8370
jutanke <jutanke@googlemail.com>
2014-06-04 15:24:03 +0200

    bla

--- bundesliga.sql
+++ bundesliga.sql
@@ -1,9 +1,8 @@
 --- bundeliga-DB by Memel, Timo, Julian
-DROP DATABASE IF EXISTS  bundesliga
 CREATE TABLE Team 
 (
 	name VARCHAR(20),
 	stadium VARCHAR(100),
-	divisionId INTEGER
-
-)
+	divisionId INTEGER,
+	CONSTRAINT team_pk PRIMARY KEY (name),
+);
